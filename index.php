fd63a70407dfacb0a56e86bf45650471039df6d6
Giuseppe Bertolini <zarabirsha@gmail.com>
2018-11-15 09:25:38 +0100

    lkndfdklnxfl

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			function add(){
 				var a = document.getElementById("a").value;
 				var b = document.getElementById("b").value;
-				var c = a + b;
+				var c = a + b + 0;
 				document.getElementById("b").value = c;
 			}
 		</script>
