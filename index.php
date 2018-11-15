748a2b68b12df42f620274e001cd60b2713ad57e
Giuseppe Bertolini <zarabirsha@gmail.com>
2018-11-15 09:38:23 +0100

    fjusufsbf

--- index.php
+++ index.php
@@ -11,14 +11,16 @@
 		<form>
 			a : <input id = "a" type = "number" name = "a">
 			b : <input id = "b" type = "number" name = "b">
-			<a href = "javascript: add()">addup</>
+			<button><a href = "javascript: add()">add</></button>
 		</form>	
+		<h1 id="sum"></h1>
 		<script>
 			function add(){
 				var a = parseInt(document.getElementById("a").value,10);
 				var b = parseInt(document.getElementById("b").value,10);
 				var c = a + b	;
-				document.getElementById("b").value = c;
+				//document.getElementById("b").value = c;
+				document.getElementById("sum").value = c;
 			}
 		</script>
 	</body>
