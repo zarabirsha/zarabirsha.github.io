651adbfb9c2f4469f45856eb42e1189b5fffbe8c
Giuseppe Bertolini <zarabirsha@gmail.com>
2018-11-15 09:31:57 +0100

    ivkiy

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			function add(){
 				var a = document.getElementById("a").value;
 				var b = document.getElementById("b").value;
-				var c = 	;
+				var c = a + b	;
 				document.getElementById("b").value = c;
 			}
 		</script>
