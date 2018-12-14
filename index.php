5ac5a58eff733d1bb12c78f0e5faad02746625a1
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-12-14 15:22:12 +0100

    beautified files

--- index.php
+++ index.php
@@ -15,42 +15,41 @@
 			Class 1996, based in Milan, I'm studying Computer Science at PoliMi
 			and this, photography, is my passion.
 			I shoot both film and digital, cameras and phone.
-			I try to portray what strikes my mind. 
+			I try to portray what strikes my mind.
 			Eyes are just tools, just like cameras and lenses.</div>
 		<div class="nav"><a href="mailto:zarabirsha@gmail.com">mail </a>-<a href="https://www.instagram.com/zarabirsha" > instagram</a></div>
 		<?php
-		$servername = "localhost";
-		$username = "readonly";
-		$dbname = "zbshaphoto";
+        $servername = "localhost";
+        $username = "readonly";
+        $dbname = "zbshaphoto";
 
-		// Create connection
-		$conn = new mysqli($servername, $username,'', $dbname);
+        // Create connection
+        $conn = new mysqli($servername, $username, '', $dbname);
 
-		$sql = "SELECT *  FROM photos";
-		$result = $conn->query($sql);
-		$images = array();
-		$descriptions = array();
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-				//echo "<div class='card'><img src='". $row["pathtofile"] . "'>";
-				//echo "<div class='description'>". $row["description"]. "</div></div>";
-				array_push($images,$row["pathtofile"]);
-				array_push($descriptions,$row["description"]);
-			}
-		} else {
-			echo "0 results";
-		}
-		$conn->close();
-		$arrlength = count($images);
+        $sql = "SELECT *  FROM photos";
+        $result = $conn->query($sql);
+        $images = array();
+        $descriptions = array();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                //echo "<div class='card'><img src='". $row["pathtofile"] . "'>";
+                //echo "<div class='description'>". $row["description"]. "</div></div>";
+                array_push($images, $row["pathtofile"]);
+                array_push($descriptions, $row["description"]);
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        $arrlength = count($images);
 
-		for($x = 0; $x < $arrlength; $x++) {
-		 	echo "<div class='card'><img id='". $x ."' src='". $images[0] ."'data='". $images[$x] ."'>";
-                        echo "<div class='description'>". $descriptions[$x]. "</div></div>";
-			
-		}
-		?>
-		
+        for ($x = 0; $x < $arrlength; $x++) {
+            echo "<div class='card'><img id='". $x ."' src='". $images[0] ."'data='". $images[$x] ."'>";
+            echo "<div class='description'>". $descriptions[$x]. "</div></div>";
+        }
+        ?>
+
 	<div class="footer">&copy 2018 Zarabirsha </div>
 		<script>
 			var lastone = 0;
@@ -64,7 +63,7 @@
 				}
 				return last;
 			}
-			
+
 			window.addEventListener('scroll', function() {
         			lastone = render(lastone);
         			//DEBUG
@@ -74,10 +73,9 @@
 			var isInViewport = function (el) {
 				const rect = el.getBoundingClientRect();
     				const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-	
+
     				return (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
 			}
 		</script>
-
 	</body>
 </html>
