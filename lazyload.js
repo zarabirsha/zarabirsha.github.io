24d3c37f23bf0905c63f50457374d44a3da430eb
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-12-22 17:56:04 +0100

    sds

--- lazyload.js
+++ lazyload.js
@@ -1,15 +1,15 @@
 const numPics = 9;
 var lastone = 1;
 var render = function(last) {
-    for (i = numpics; i >= last; i--){
-        if(isInViewport(document.getElementById(i))){
-            for(j = i+1; j >= last; j--){
+    for (i = numPics; i >= last; i--) {
+        if (isInViewport(document.getElementById(i))) {
+            for (j = i + 1; j >= last; j--) {
                 document.getElementById(Math.min(j, numPics - 1)).src = document.getElementById(Math.min(j, numPics - 1)).getAttribute("data");
             }
-            last=i+1;
+            last = i + 1;
         }
     }
-    
+
     return last;
 };
 
